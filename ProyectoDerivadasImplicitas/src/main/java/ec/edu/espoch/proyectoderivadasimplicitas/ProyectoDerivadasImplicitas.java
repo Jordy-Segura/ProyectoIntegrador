7ad855cc46fc17caec2be955db2d923b66eef70e
jordy <marcelo2005jmsp@gmail.com>
2025-01-27 23:20:01 -0500

    proyecto creado

---
+++ ProyectoDerivadasImplicitas/src/main/java/ec/edu/espoch/proyectoderivadasimplicitas/ProyectoDerivadasImplicitas.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.proyectoderivadasimplicitas;
+
+/**
+ *
+ * @author jordy
+ */
+public class ProyectoDerivadasImplicitas {
+
+    public static void main(String[] args) {
+        Interfaz Derivadas=new Interfaz();
+        Derivadas.setVisible(true);
+    }
+}
